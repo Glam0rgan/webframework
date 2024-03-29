71e7be449f6a32a7dce4efaa33ea2239fe5e0792
Glam0rgan <1742771620@qq.com>
2024-03-29 14:24:55 +0800

    day4

--- main.go
+++ main.go
@@ -8,23 +8,35 @@ import (
 
 func main() {
 	r := gwf.New()
-	r.GET("/", func(c *gwf.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *gwf.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
 
-	r.GET("/hello", func(c *gwf.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gwf.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gwf</h1>")
+		})
 
-	r.GET("/hello/:name", func(c *gwf.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *gwf.Context) {
+			// expect /hello?name=gwf
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
 
-	r.GET("/assets/*filepath", func(c *gwf.Context) {
-		c.JSON(http.StatusOK, gwf.H{"filepath": c.Param("filepath")})
-	})
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gwf.Context) {
+			// expect /hello/gwf
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gwf.Context) {
+			c.JSON(http.StatusOK, gwf.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
